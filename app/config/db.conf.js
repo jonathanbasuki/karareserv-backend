98012863c77501f9b3ab1551d834ca724ef4328e
jonathanbasuki <basukijonathan4@gmail.com>
2025-06-21 10:13:54 +0700

    build: add database config and associations

--- app/config/db.conf.js
+++ app/config/db.conf.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
     host: process.env.DB_HOST,
     dialect: process.env.DB_PREFIX
-  },
-  production: {
-    username: process.env.DB_USER_PROD,
-    password: process.env.DB_PASS_PROD,
-    database: process.env.DB_NAME_PROD,
-    host: process.env.DB_HOST_PROD,
-    dialect: process.env.DB_PREFIX_PROD
   }
+);
+
+module.exports = {
+  sequelize,
+  Sequelize
 };
